cd61f9b1ce037902b2d070bb780bbe2123016998
Tayyeb Israr <tayyebisrar@users.noreply.github.com>
2025-06-09 19:42:02 +0100

    Optimise SSE2 Grayscale

--- src/sse2f.cpp
+++ src/sse2f.cpp
@@ -11,6 +11,7 @@ void filter_grayscale(IMAGE_DATA &image){
     const __m128i red_multiplier = _mm_set1_epi16(77); // roughly 0.2989 * 256
     const __m128i green_multiplier = _mm_set1_epi16(150); // roughly 0.5870 * 256
     const __m128i blue_multiplier = _mm_set1_epi16(29); // roughly 0.1140 * 256
+    const __m128i simdzero = _mm_setzero_si128();
 
     for (int i=0; i<totalloops; i++){
         // integer division by 3 is not possible, so will use grayscale = 0.2989×R+0.5870×G+0.1140×B 
@@ -20,12 +21,12 @@ void filter_grayscale(IMAGE_DATA &image){
         __m128i blue8 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(image.blue.data() + i*16));
 
         // convert to 16 bit integers because otherwise the multiplication would overflow
-        __m128i red16l = _mm_unpacklo_epi8(red8, _mm_setzero_si128());
-        __m128i green16l = _mm_unpacklo_epi8(green8, _mm_setzero_si128());
-        __m128i blue16l = _mm_unpacklo_epi8(blue8, _mm_setzero_si128());
-        __m128i red16h = _mm_unpackhi_epi8(red8, _mm_setzero_si128());
-        __m128i green16h = _mm_unpackhi_epi8(green8, _mm_setzero_si128());
-        __m128i blue16h = _mm_unpackhi_epi8(blue8, _mm_setzero_si128());
+        __m128i red16l = _mm_unpacklo_epi8(red8, simdzero);
+        __m128i green16l = _mm_unpacklo_epi8(green8, simdzero);
+        __m128i blue16l = _mm_unpacklo_epi8(blue8, simdzero);
+        __m128i red16h = _mm_unpackhi_epi8(red8, simdzero);
+        __m128i green16h = _mm_unpackhi_epi8(green8, simdzero);
+        __m128i blue16h = _mm_unpackhi_epi8(blue8, simdzero);
 
         // so for example red16l looks like r0, 0, r1, 0, r2, 0, r3, 0, r4, 0, r5, 0, r6, 0, r7, 0
         // and red16h looks like r8, 0, r9, 0, r10, 0, r11, 0, r12, 0, r13, 0, r14, 0, r15, 0
